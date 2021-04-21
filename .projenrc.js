8738f498e4527a966b71302417e27ce3472d2f3c
benkajaja <ny343478@gmail.com>
2021-04-22 00:04:15 +0800

    Change cdk version to 1.100.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.100.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'cdk-ncs-reminder',
