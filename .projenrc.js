5dfb7949f6d2cac7fb538a7ac4fdab1957a2a5c8
benkajaja <ny343478@gmail.com>
2021-05-26 22:03:09 +0800

    Upgrade package

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.100.0',
+  cdkVersion: '1.106.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'cdk-ncs-reminder',
